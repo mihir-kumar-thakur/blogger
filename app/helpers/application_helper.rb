7e9249cd5c125554364f12a344a875032733482f
Mihir Kumar Thakur <mihir@soulcurry.co>
2019-12-23 11:25:11 +0530

    Changes with post shows views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,10 @@ module ApplicationHelper
       flash_type.to_s
     end
   end
+
+  def text_with_badge(text, badge_value = nil)
+    badge = content_tag :span, badge_value, class: 'badge'
+    text = raw "#{text} #{badge}" if badge_value
+    return text
+  end
 end
